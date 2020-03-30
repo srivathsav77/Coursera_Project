c7b55488f544c9fbcdd3864e2e6c911a1a8bbcbf
srivathsav77 <49104256+srivathsav77@users.noreply.github.com>
2020-03-31 02:53:37 +0530

    Update Coursera_Project.r

--- Coursera_Project.r
+++ Coursera_Project.r
@@ -1,4 +1,3 @@
-
 getwd()
 
 setwd("C:/Users/Srivathsav/Desktop")
@@ -11,46 +10,34 @@ download.file(fileUrl,"C:/Users/Srivathsav/Desktop/samsung.zip")
 
 unzip("C:/Users/Srivathsav/Desktop/samsung.zip")
 
-
-
 #Feature and Activity names 
 Features=read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/features.txt")
 
-
 Features=read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/features.txt")$V2
-
 
 Activity_Labels=read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/activity_labels.txt")
 
-
 Activity_Labels=read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/activity_labels.txt")$V2
-
 
 
 #Test Data
 subject_test<-read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/test/subject_test.txt",col.names = "subject_id")
 
+X_test<-read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/test/X_test.txt",col.names = "Features")
 
-X_test=read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/test/X_test.txt",col.names = Features)
-
-Y_test
-head(Y_test)
 Y_test<-read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/test/y_test.txt",col.names = "Activity_Labels")
-
 
 Test_Data<- cbind(subject_test,X_test,Y_test)
 
 #Train Data
-
-
 subject_train<-read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/train/subject_train.txt", col.names = "subject_id")
-X_train<-read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/train/X_train.txt", col.names = Features)
+
+X_train<-read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/train/X_train.txt", col.names = "Features")
 
 Y_train<-read.table("C:/Users/Srivathsav/Desktop/UCI HAR Dataset/train/y_train.txt", col.names = "Activity_Labels")
 
-
 Train_Data<- cbind(subject_train,X_train,Y_train)
 
-
 #Combined Data
+
 m<- rbind(Train_Data,Test_Data)
